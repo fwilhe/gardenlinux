e645761417124e4f317ddf36253d631ccadf1151
Florian Wilhelm <florian.wilhelm02@sap.com>
2024-04-10 15:16:35 +0200

    commit_id_short

--- features/lima/generate-manifest.py
+++ features/lima/generate-manifest.py
@@ -59,8 +59,10 @@ if __name__ == '__main__':
     print(args.gardenlinux_version)
     print(args.commit_id)
 
-    disk_image_name_amd64 = disk_image_name(args.cname_base, 'amd64', args.gardenlinux_version, args.commit_id)
-    disk_image_name_arm64 = disk_image_name(args.cname_base, 'arm64', args.gardenlinux_version, args.commit_id)
+    commit_id_short = args.commit_id[0:8]
+
+    disk_image_name_amd64 = disk_image_name(args.cname_base, 'amd64', args.gardenlinux_version, commit_id_short)
+    disk_image_name_arm64 = disk_image_name(args.cname_base, 'arm64', args.gardenlinux_version, commit_id_short)
 
     sha_amd64 = ""
     sha_arm64 = ""
@@ -70,8 +72,8 @@ if __name__ == '__main__':
     with open(disk_image_name_arm64, 'rb', buffering=0) as f:
         sha_arm64 = hashlib.file_digest(f, 'sha256').hexdigest()
 
-    yaml_name_amd64 = yaml_name(args.cname_base, 'amd64', args.gardenlinux_version, args.commit_id)
-    yaml_name_arm64 = yaml_name(args.cname_base, 'arm64', args.gardenlinux_version, args.commit_id)
+    yaml_name_amd64 = yaml_name(args.cname_base, 'amd64', args.gardenlinux_version, commit_id_short)
+    yaml_name_arm64 = yaml_name(args.cname_base, 'arm64', args.gardenlinux_version, commit_id_short)
 
     manifest = template\
         .replace("__DISK_IMAGE_NAME_AMD__", disk_image_name_amd64)\
@@ -82,5 +84,5 @@ if __name__ == '__main__':
         .replace("__SHA_ARM__", sha_arm64)\
         .replace("__VERSION__", args.gardenlinux_version)
 
-    with open(output_file_name(args.cname_base, args.gardenlinux_version, args.commit_id), "w+") as file:
+    with open(output_file_name(args.cname_base, args.gardenlinux_version, commit_id_short), "w+") as file:
         file.write(manifest)
